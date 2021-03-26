8bd4b84a912bc893dfea1ad79bd90e426a4bd6ab
Sean McGinty <31000120+s3ansh33p@users.noreply.github.com>
2021-03-26 08:36:21 +0800

    ✨ Change from testing environment to production

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const App = new (require('./src/classes/App'));
 const Logger = require('./src/util/Logger');
-// const Cron = require('./src/util/cron');
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const Cron = require('./src/util/cron');
 
 (async function () {
     await App.registerRoutes();
     await App.listen(() => {
         Logger.info(`Express started on on port ${process.env.EXPRESS_PORT}`);
     }, true);
-    // Cron.setCron();
+    Cron.setCron();
 })();
