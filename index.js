04592fb2dbe5179dbda8da4231a7f37ed4f3b31b
Crispy <frostyid1@gmail.com>
2021-04-10 20:38:53 +1000

    1.0.2
    bug fixes & addons

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const App = new (require('./src/classes/App'));
 const Logger = require('./src/util/Logger');
 const Cron = require('./src/util/cron');
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 (async function () {
     await App.registerRoutes();
     await App.listen(() => {
         Logger.info(`Express started on on port ${process.env.EXPRESS_PORT}`);
     }, true);
     Cron.setCron();
-     Cron.autoRunMYSQL();
+    // Cron.autoRunMYSQL();
 })()
