93a6773e2caeba9f74a980e07bdbee271342719f
Sean McGinty <31000120+s3ansh33p@users.noreply.github.com>
2021-05-14 17:58:59 +0800

    Update internal api for stats

--- src/routes/api.js
+++ src/routes/api.js
@@ -44,6 +44,8 @@ class API extends Router {
                     },
                     totalBotsOnline: statistics.data.totalBotsOnline,
                     totalBotsSendingTelemetry: statistics.data.totalBotsSendingTelemetry,
+                    totalRegistered: statistics.totalRegistered,
+                    activeInLastTwoDays: statistics.activeInLastTwoDays,
                     id: statistics.data.pid,
                     serverTime: serverTime
                 }
